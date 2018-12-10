309d6ec3446ad8daf5b0793bd76b012a7caca299
Ailton <ailton.salg@gmail.com>
2018-12-10 18:59:29 -0500

    Servidor con express

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8000, function() {
+    console.log('Server port on 8000');
+});
